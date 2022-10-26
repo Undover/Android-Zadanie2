531ccbd14c9d85054ba6ed0edf25443742a8d16c
sokaries <sokaries1@gmail.com>
2022-10-26 02:33:25 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mainactivity/TaskListActivity.java
@@ -0,0 +1,16 @@
+package com.example.mainactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import java.util.UUID;
+
+public class TaskListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return new TaskListFragment();
+    }
+}
---
+++ app/src/main/java/com/example/mainactivity/TaskStorage.java
@@ -0,0 +1,38 @@
+package com.example.mainactivity;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class TaskStorage {
+    private static final TaskStorage taskStorage = new TaskStorage();
+
+    private final List<Task> tasks;
+
+    public static TaskStorage getInstance() {
+        return taskStorage;
+    }
+
+    private TaskStorage() {
+        tasks = new ArrayList<>();
+        for (int i = 0; i <= 150; i++) {
+            Task task = new Task();
+            task.setName("Pilne zadanie numer " + i);
+            task.setDone(i % 3 == 0);
+            tasks.add(task);
+        }
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public Task getTask(UUID id) {
+        for (Task task : tasks) {
+            if (task.getId().equals(id)) {
+                return task;
+            }
+        }
+        return null;
+    }
+}
